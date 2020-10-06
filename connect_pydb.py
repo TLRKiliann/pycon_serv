43b87dbb79b23453683e54240d5e999beb749bbb
TLRKiliann <philogenie@protonmail.com>
2020-10-06 16:52:23 +0200

    update

--- connect_pydb.py
+++ connect_pydb.py
@@ -18,6 +18,32 @@ class ConnectorDB:
 
         MainFrame = Frame(self.root, bd=10, width=770, height=False)
 
+
+
+        scroll_y=Scrollbar(LeftFrame, orient=VERTICAL)
+
+        self.student_records=ttk.Treeview(LeftFrame, height=12, columns=("stdid",
+            "firstname", "surname", "address", "gender", "mobile"),yscrollcommand=scroll_y.set)
+        scroll_y.pack(side=RIGHT, fill=Y)
+
+        self.student_records.heading("stdid", text="StudentID.")
+        self.student_records.heading("firstname", text="Firstname")
+        self.student_records.heading("surname", text="Surname")
+        self.student_records.heading("address", text="Address")
+        self.student_records.heading("gender", text="Gender")
+        self.student_records.heading("mobile", text="Mobile")
+
+        self.student_records['show']="headings"
+
+        self.student_records.column("stdid", width=70)
+        self.student_records.column("firstname", width=70)
+        self.student_records.column("surname", width=70)
+        self.student_records.column("address", width=70)
+        self.student_records.column("gender", width=70)
+        self.student_records.column("mobile", width=70)
+
+        self.student_records.pack(fill=BOTH, expand=1)
+
 if __name__=='__main__':
     root = Tk()
     application = ConnectorDB(root)
